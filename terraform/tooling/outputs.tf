c852341c46f7621f9630a847a2c1f7c1cde6e9c2
Olalekan Ogundare <flexcloud2019@gmail.com>
2026-06-29 15:45:01 -0400

    Updating Tooling Workflow

--- terraform/tooling/outputs.tf
+++ terraform/tooling/outputs.tf
@@ -1,19 +1,29 @@
+# output "bootstrap_role_arn" {
+#   value = aws_iam_role.react-app-tooling-bootstrap.arn
+# }
+
+# output "terraform_state_bucket" {
+#   value = aws_s3_bucket.state-file-bucket.bucket
+# }
+
+# output "terraform_lock_table" {
+#   value = aws_dynamodb_table.state-file-locks.name
+# }
+
+# output "terraform_state_kms_key_id" {
+#   value = aws_kms_alias.state-file-key-alias.name
+# }
+
+# output "terraform_state_kms_key_arn" {
+#   value = aws_kms_key.state-file-key.arn
+# }
+
+
+
+output "openid_connect_provider_arn" {
+  value = aws_iam_openid_connect_provider.github-react-app.arn
+}
+
 output "bootstrap_role_arn" {
   value = aws_iam_role.react-app-tooling-bootstrap.arn
-}
-
-output "terraform_state_bucket" {
-  value = aws_s3_bucket.state-file-bucket.bucket
-}
-
-output "terraform_lock_table" {
-  value = aws_dynamodb_table.state-file-locks.name
-}
-
-output "terraform_state_kms_key_id" {
-  value = aws_kms_alias.state-file-key-alias.name
-}
-
-output "terraform_state_kms_key_arn" {
-  value = aws_kms_key.state-file-key.arn
 }
