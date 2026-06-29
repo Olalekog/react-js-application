19f887606c040160a250fbc5aa7084b9bc0f9619
Olalekan Ogundare <flexcloud2019@gmail.com>
2026-06-29 14:27:46 -0400

    Updating Tooling Workflow

--- terraform/tooling/outputs.tf
+++ terraform/tooling/outputs.tf
@@ -1,19 +1,19 @@
-output "bootstrap_role_arn" {
-  value = aws_iam_role.tooling-bootstrap.arn
-}
+# output "bootstrap_role_arn" {
+#   value = aws_iam_role.tooling-bootstrap.arn
+# }
 
-output "terraform_state_bucket" {
-  value = aws_s3_bucket.state-file-bucket.bucket
-}
+# output "terraform_state_bucket" {
+#   value = aws_s3_bucket.state-file-bucket.bucket
+# }
 
-output "terraform_lock_table" {
-  value = aws_dynamodb_table.state-file-locks.name
-}
+# output "terraform_lock_table" {
+#   value = aws_dynamodb_table.state-file-locks.name
+# }
 
-output "terraform_state_kms_key_id" {
-  value = aws_kms_alias.state-file-key-alias.name
-}
+# output "terraform_state_kms_key_id" {
+#   value = aws_kms_alias.state-file-key-alias.name
+# }
 
-output "terraform_state_kms_key_arn" {
-  value = aws_kms_key.state-file-key.arn
-}
+# output "terraform_state_kms_key_arn" {
+#   value = aws_kms_key.state-file-key.arn
+# }
--- terraform/tooling/terraform.tfvars
+++ terraform/tooling/terraform.tfvars
@@ -2,7 +2,7 @@ aws_region         = "us-east-1"
 tooling_account_id = "866934333672"
 project_name       = "react-js-application"
 
-# GitHub repository owner and repository name.
-# Do not create GitHub variables that start with GITHUB_; use GH_ORG and GH_REPO in GitHub Actions.
-github_org  = "Olalekog"
-github_repo = "react-js-application"
+# # GitHub repository owner and repository name.
+# # Do not create GitHub variables that start with GITHUB_; use GH_ORG and GH_REPO in GitHub Actions.
+# github_org  = "Olalekog"
+# github_repo = "react-js-application"
