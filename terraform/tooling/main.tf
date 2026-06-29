955bc71fc87a18468241b51726f26f73bf7ffb1e
Olalekan Ogundare <flexcloud2019@gmail.com>
2026-06-29 16:35:41 -0400

    Updating Tooling Workflow

--- terraform/tooling/main.tf
+++ terraform/tooling/main.tf
@@ -93,7 +93,7 @@ resource "aws_iam_role" "bootstrap" {
     Statement = [{
       Effect = "Allow"
       Principal = {
-        Federated = aws_iam_openid_connect_provider.github.arn
+        Federated = aws_iam_openid_connect_provider.github-oidc-connection.arn
       }
       Action = "sts:AssumeRoleWithWebIdentity"
       Condition = {
