5af321317b35e79c10b2487d9f3a3fec5e1b3f0b
Olalekan Ogundare <flexcloud2019@gmail.com>
2026-06-29 12:13:22 -0400

    Adding Dev Push Workflow

--- terraform/tooling/variables.tf
+++ terraform/tooling/variables.tf
@@ -3,4 +3,4 @@ variable "tooling_account_id" { type = string }
 variable "project_name" { type = string }
 variable "github_org" { type = string }
 variable "github_repo" { type = string }
-variable "tags" { type = map(string) default = {} }
+variable "tags" { type = map(string) default = {}
