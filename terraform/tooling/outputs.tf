91cb9f88f251e23818cc7068ceacf0d26ff5a1ad
Olalekan Ogundare <flexcloud2019@gmail.com>
2026-06-29 15:48:42 -0400

    Updating Tooling Workflow

--- terraform/tooling/outputs.tf
+++ terraform/tooling/outputs.tf
@@ -2,21 +2,21 @@
 #   value = aws_iam_role.react-app-tooling-bootstrap.arn
 # }
 
-# output "terraform_state_bucket" {
-#   value = aws_s3_bucket.state-file-bucket.bucket
-# }
+output "terraform_state_bucket" {
+  value = aws_s3_bucket.state-file-bucket.bucket
+}
 
-# output "terraform_lock_table" {
-#   value = aws_dynamodb_table.state-file-locks.name
-# }
+output "terraform_lock_table" {
+  value = aws_dynamodb_table.state-file-locks.name
+}
 
-# output "terraform_state_kms_key_id" {
-#   value = aws_kms_alias.state-file-key-alias.name
-# }
+output "terraform_state_kms_key_id" {
+  value = aws_kms_alias.state-file-key-alias.name
+}
 
-# output "terraform_state_kms_key_arn" {
-#   value = aws_kms_key.state-file-key.arn
-# }
+output "terraform_state_kms_key_arn" {
+  value = aws_kms_key.state-file-key.arn
+}
 
 
 
