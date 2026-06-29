7936ba2802c824ae2ff6e54ad0c789964b32699e
Olalekan Ogundare <flexcloud2019@gmail.com>
2026-06-29 13:48:58 -0400

    Correcting error

--- terraform/tooling/main.tf
+++ terraform/tooling/main.tf
@@ -77,7 +77,7 @@ resource "aws_dynamodb_table" "locks" {
   })
 }
 
-resource "aws_iam_openid_connect_provider" "github" {
+resource "aws_iam_openid_connect_provider" "github-rjs" {
   url             = "https://token.actions.githubusercontent.com"
   client_id_list  = ["sts.amazonaws.com"]
   thumbprint_list = ["6938fd4d98bab03faadb97b34396831e3780aea1"]
