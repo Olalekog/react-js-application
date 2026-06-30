33950cd4437680fe53d22d7cdb4c072776746d51
Olalekan Ogundare <flexcloud2019@gmail.com>
2026-06-30 08:54:19 -0400

    Disabling Tooling Deployment

--- terraform/tooling/outputs.tf
+++ terraform/tooling/outputs.tf
@@ -1,5 +1,5 @@
-output "bootstrap_role_arn" { value = aws_iam_role.bootstrap.arn }
-output "terraform_state_bucket" { value = aws_s3_bucket.state.bucket }
-output "terraform_lock_table" { value = aws_dynamodb_table.locks.name }
-output "terraform_state_kms_key_id" { value = aws_kms_alias.state.name }
-output "terraform_state_kms_key_arn" { value = aws_kms_key.state.arn }
+# output "bootstrap_role_arn" { value = aws_iam_role.bootstrap.arn }
+# output "terraform_state_bucket" { value = aws_s3_bucket.state.bucket }
+# output "terraform_lock_table" { value = aws_dynamodb_table.locks.name }
+# output "terraform_state_kms_key_id" { value = aws_kms_alias.state.name }
+# output "terraform_state_kms_key_arn" { value = aws_kms_key.state.arn }
