7148f59f84ca42436ff9cd2ebf88f0ea77968fbc
Olalekan Ogundare <flexcloud2019@gmail.com>
2026-07-01 09:11:40 -0400

    Fix bootstrap variables and backend config

--- terraform/bootstrap/outputs.tf
+++ terraform/bootstrap/outputs.tf
@@ -1,10 +1,10 @@
-output "deploy_role_arns" {
-  value = {
-    dev        = module.dev_environment_bootstrap.deploy_role_arn
-    uat        = module.uat_environment_bootstrap.deploy_role_arn
-    production = module.production_environment_bootstrap.deploy_role_arn
-  }
-}
+# output "deploy_role_arns" {
+#   value = {
+#     dev        = module.dev_environment_bootstrap.deploy_role_arn
+#     uat        = module.uat_environment_bootstrap.deploy_role_arn
+#     production = module.production_environment_bootstrap.deploy_role_arn
+#   }
+# }
 
 output "frontend_ecr_repository_urls" {
   value = {
