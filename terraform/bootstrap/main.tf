234d78946fb6c9aa1da76a237de1a5641a5e7886
Olalekan Ogundare <flexcloud2019@gmail.com>
2026-06-30 23:23:48 -0400

    Format Terraform bootstrap files

--- terraform/bootstrap/main.tf
+++ terraform/bootstrap/main.tf
@@ -6,16 +6,16 @@ locals {
 }
 
 module "dev_environment_bootstrap" {
-  source = "../modules/env-bootstrap"
+  source = "./modules/env-bootstrap"
 
-  project_name                = var.project_name
-  environment                 = "dev"
-  account_id                  = var.aws_account_id
-  tooling_account_id           = var.tooling_account_id
-  bootstrap_role_arn           = var.bootstrap_role_arn
-  terraform_state_bucket       = var.terraform_state_bucket
-  terraform_lock_table         = var.terraform_lock_table
-  terraform_state_kms_key_arn  = var.terraform_state_kms_key_arn
+  project_name               = var.project_name
+  environment                = "dev"
+  account_id                 = var.aws_account_id
+  tooling_account_id         = var.tooling_account_id
+  bootstrap_role_arn         = var.bootstrap_role_arn
+  terraform_state_bucket     = var.terraform_state_bucket
+  terraform_lock_table       = var.terraform_lock_table
+  terraform_state_kms_key_arn = var.terraform_state_kms_key_arn
 
   tags = merge(local.common_tags, {
     Environment = "dev"
@@ -23,16 +23,16 @@ module "dev_environment_bootstrap" {
 }
 
 module "uat_environment_bootstrap" {
-  source = "../modules/env-bootstrap"
+  source = "./modules/env-bootstrap"
 
-  project_name                = var.project_name
-  environment                 = "uat"
-  account_id                  = var.aws_account_id
-  tooling_account_id           = var.tooling_account_id
-  bootstrap_role_arn           = var.bootstrap_role_arn
-  terraform_state_bucket       = var.terraform_state_bucket
-  terraform_lock_table         = var.terraform_lock_table
-  terraform_state_kms_key_arn  = var.terraform_state_kms_key_arn
+  project_name               = var.project_name
+  environment                = "uat"
+  account_id                 = var.aws_account_id
+  tooling_account_id         = var.tooling_account_id
+  bootstrap_role_arn         = var.bootstrap_role_arn
+  terraform_state_bucket     = var.terraform_state_bucket
+  terraform_lock_table       = var.terraform_lock_table
+  terraform_state_kms_key_arn = var.terraform_state_kms_key_arn
 
   tags = merge(local.common_tags, {
     Environment = "uat"
@@ -40,16 +40,16 @@ module "uat_environment_bootstrap" {
 }
 
 module "production_environment_bootstrap" {
-  source = "../modules/env-bootstrap"
+  source = "./modules/env-bootstrap"
 
-  project_name                = var.project_name
-  environment                 = "production"
-  account_id                  = var.aws_account_id
-  tooling_account_id           = var.tooling_account_id
-  bootstrap_role_arn           = var.bootstrap_role_arn
-  terraform_state_bucket       = var.terraform_state_bucket
-  terraform_lock_table         = var.terraform_lock_table
-  terraform_state_kms_key_arn  = var.terraform_state_kms_key_arn
+  project_name               = var.project_name
+  environment                = "production"
+  account_id                 = var.aws_account_id
+  tooling_account_id         = var.tooling_account_id
+  bootstrap_role_arn         = var.bootstrap_role_arn
+  terraform_state_bucket     = var.terraform_state_bucket
+  terraform_lock_table       = var.terraform_lock_table
+  terraform_state_kms_key_arn = var.terraform_state_kms_key_arn
 
   tags = merge(local.common_tags, {
     Environment = "production"
